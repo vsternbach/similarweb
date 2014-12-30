eca37ba93f83889318e47d53e3a18f5154014289
Vlad Sternbach <gr82bu@gmail.com>
2014-12-30 16:35:27 +0200

    Update test,universal-search via Frontify

---
+++ frontify/modules/test/js/test.js
@@ -0,0 +1,8 @@
+angular.module('test', []).directive('test', function () {
+  return {
+    templateUrl: '../test.html'
+    controller: function($scope) {
+    $scope.greeting = 'test';
+    }
+  };
+});
